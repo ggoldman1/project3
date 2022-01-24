c3b10d8270b95fc4f816c4509e8bec9fcc47ba6e
Grant Goldman <grant.goldman@ucsf.edu>
2022-01-24 10:52:03 -0800

    add student test

--- test/test_mst.py
+++ test/test_mst.py
@@ -63,4 +63,26 @@ def test_mst_single_cell_data():
 
 def test_mst_student():
     """ TODO: Write at least one unit test for MST construction """
-    pass
+    adj_mat = [[0, 40, 14, 52, 38, 79, 53, 66, 72, 55],
+               [40,  0, 44, 25, 81, 34, 54, 59, 56, 65],
+               [14, 44, 0, 64, 79, 50, 73, 71, 55, 44],
+               [52, 25, 64,  0, 43, 75, 49, 20, 65, 48],
+               [38, 81, 79, 43, 0, 71, 48, 38, 40, 33],
+               [79, 34, 50, 75, 71, 0, 13, 64, 34, 47],
+               [53, 54, 73, 49, 48, 13, 0, 81,  9, 73],
+               [66, 59, 71, 20, 38, 64, 81, 0, 19, 55],
+               [72, 56, 55, 65, 40, 34,  9, 19, 0, 28],
+               [55, 65, 44, 48, 33, 47, 73, 55, 28, 0]]
+
+    g = Graph(np.array(adj_mat))
+    g.construct_mst()
+    check_mst(g.adj_mat, g.mst, 199)
+
+    adj_mat = [[1, 1, 3, 4, 4],
+               [1, 0, 2, 3, 1],
+               [3, 2, 2, 2, 1],
+               [4, 3, 2, 3, 2],
+               [4, 1, 1, 2, 1]]
+    g = Graph(np.array(adj_mat))
+    g.construct_mst()
+    check_mst(g.adj_mat, g.mst, 5)
